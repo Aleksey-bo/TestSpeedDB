76e1bd87a73b7ad77997099e67de8f89621b6136
Aleksey-bo <gvozdea1243@gmail.com>
2025-02-17 05:51:04 -0600

    test commit

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 
-# engine = create_async_engine('sqlite+aiosqlite:///./freelance.db', echo=True)
-engine = create_async_engine('postgresql+asyncpg://postgres:124235768@localhost:5432/freelance', echo=True)
+engine = create_async_engine('', echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
